c96886c83ae78a0d70bae3135ba41d1f1ec0cc39
Stefano Corra <stefano.corra@hotmail.com>
2022-12-30 22:30:08 -0500

    post commit

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
+var items = []
 
+// setup for ejs
 app.set('view engine', 'ejs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// show the current date
 app.get('/', function (req, res) {
   var today = new Date()
 
@@ -13,10 +18,23 @@ app.get('/', function (req, res) {
     day: 'numeric',
     month: 'long',
   }
-
+  // format the date and add to variable
   var day = today.toLocaleDateString('en-US', options)
 
-  res.render('list', { kindOfDay: day })
+  // render all res
+  res.render('list', { kindOfDay: day, newListItem: items })
+})
+
+// add Items to the list
+app.post('/', function (req, res) {
+  //add item value from input to the variable
+  var item = req.body.newItem
+
+  // add item to the array
+  items.push(item)
+
+  // redirect res
+  res.redirect('/')
 })
 
 app.listen(3000, function () {
